d4063865226aa1505b6ff282f0c588f95c6aae04
Joshua Umahi <98918202+Renbo-Studio@users.noreply.github.com>
2023-03-17 14:18:03 +0100

    All Major Fixes
    I Need Data 😪

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,77 +2,111 @@ const body = document.body;
 const menu = body.querySelector(".menu");
 const menuItems = menu.querySelectorAll(".menu__item");
 const menuBorder = menu.querySelector(".menu__border");
-let activeItem = menu.querySelector(".active");
+let activeItem = document.querySelector(".active");
+const modalClose = document.querySelector("#close");
+let modal = document.querySelector(".modal");
+let modalOpen = document.querySelector(".globe");
+let appMode = "appMode";
 
 function clickItem(item, index) {
+  menu.style.removeProperty("--timeOut");
 
-    menu.style.removeProperty("--timeOut");
-    
-    if (activeItem == item) return;
-    
-    if (activeItem) {
-        activeItem.classList.remove("active");
-    }
+  if (activeItem == item) return;
 
-    
-    item.classList.add("active");
-    activeItem = item;
-    offsetMenuBorder(activeItem, menuBorder);
-    
-    
+  if (activeItem) {
+    activeItem.classList.remove("active");
+  }
+
+  item.classList.add("active");
+  activeItem = item;
+  offsetMenuBorder(activeItem, menuBorder);
 }
 
 function offsetMenuBorder(element, menuBorder) {
-
-    const offsetActiveItem = element.getBoundingClientRect();
-    const left = Math.floor(offsetActiveItem.left - menu.offsetLeft - (menuBorder.offsetWidth  - offsetActiveItem.width) / 2) +  "px";
-    menuBorder.style.transform = `translate3d(${left}, 0 , 0)`;
-
+  const offsetActiveItem = element.getBoundingClientRect();
+  const left =
+    Math.floor(
+      offsetActiveItem.left -
+        menu.offsetLeft -
+        (menuBorder.offsetWidth - offsetActiveItem.width) / 2
+    ) + "px";
+  menuBorder.style.transform = `translate3d(${left}, 0 , 0)`;
 }
 
 offsetMenuBorder(activeItem, menuBorder);
 
 menuItems.forEach((item, index) => {
+  item.addEventListener("click", () => clickItem(item, index));
+});
 
-    item.addEventListener("click", () => clickItem(item, index));
-    
-})
-
-const modeToggle = document.querySelector('#mode-toggle');
-const bodyTheme = document.querySelector('body');
+const modeToggle = document.querySelector("#mode-toggle");
+const bodyTheme = document.querySelector("body");
 
 function modeSwitch() {
-    const currentBgColor = getComputedStyle(bodyTheme).backgroundColor;
-    console.log(currentBgColor);
-  
-    if (currentBgColor === 'rgb(28, 28, 28)') {
-      bodyTheme.style.backgroundColor = 'white';
-      document.querySelectorAll('.m-name').forEach(elem => elem.style.color = 'black');
-      document.querySelectorAll('.menu__item').forEach(elem => elem.style.color = 'black');
-      document.querySelectorAll('.m-options').forEach(elem => elem.style.color = 'black');
-      document.querySelectorAll('.icon').forEach(elem => elem.style.stroke = 'black');
-      document.querySelector('.top-effect').style.background = 'transparent';
-      document.querySelector('.menu').style.background = '#f5f5f5';
-    } else {
-      bodyTheme.style.backgroundColor = 'rgb(28, 28, 28)';
-      document.querySelectorAll('.m-name').forEach(elem => elem.style.color = 'white');
-      document.querySelectorAll('.menu__item').forEach(elem => elem.style.color = 'white');
-      document.querySelectorAll('.m-options').forEach(elem => elem.style.color = 'white');
-      document.querySelectorAll('.icon').forEach(elem => elem.style.stroke = 'white');
-    //   document.querySelector('.menu__icon.active > icon').style.stroke = 'black';
-        const s =  document.getElementsByClassName('active')[0];
-    console.log(s);
-      document.querySelector('.top-effect').style.background = 'linear-gradient(transparent, transparent, #1c1c1c)';
-      document.querySelector('.d-title').style.color = '#fff';
-      document.querySelector('.menu').style.background = '#1c1c1c';
-    }
-  }
-  
-  
-  
+  const currentBgColor = getComputedStyle(bodyTheme).backgroundColor;
 
+  if (currentBgColor === "rgb(28, 28, 28)") {
+    lightMode();
+  } else {
+    darkMode();
+  }
+}
+
+function darkMode() {
+  bodyTheme.style.backgroundColor = "rgb(28, 28, 28)";
+  document
+    .querySelectorAll(".m-name")
+    .forEach((elem) => (elem.style.color = "white"));
+  document
+    .querySelectorAll(".menu__item")
+    .forEach((elem) => (elem.style.color = "white"));
+  document
+    .querySelectorAll(".m-options")
+    .forEach((elem) => (elem.style.color = "white"));
+  document
+    .querySelectorAll(".icon")
+    .forEach((elem) => (elem.style.color = "white"));
+  document.querySelector(".top-effect").style.background =
+    "linear-gradient(transparent, transparent, #1c1c1c)";
+  document.querySelector(".d-title").style.color = "#fff";
+  document.querySelector(".menu").style.background = "#1c1c1c";
+  localStorage.setItem(appMode, "dark");
+}
+
+function lightMode() {
+  bodyTheme.style.backgroundColor = "white";
+  document
+    .querySelectorAll(".m-name")
+    .forEach((elem) => (elem.style.color = "black"));
+  document
+    .querySelectorAll(".menu__item")
+    .forEach((elem) => (elem.style.color = "black"));
+  document
+    .querySelectorAll(".m-options")
+    .forEach((elem) => (elem.style.color = "black"));
+  document
+    .querySelectorAll(".icon")
+    .forEach((elem) => (elem.style.color = "black"));
+  document.querySelector(".top-effect").style.background = "transparent";
+  document.querySelector(".menu").style.background = "#f5f5f5";
+  localStorage.setItem(appMode, "light");
+}
+
+if (localStorage.getItem(appMode) === "dark") {
+  darkMode();
+} else {
+  lightMode();
+}
+
+function modalFuc() {
+  modal.classList.toggle("hidden");
+  console.log(modal.classList);
+}
+
+modalClose.addEventListener('click', modalFuc);
+modalOpen.addEventListener('click', modalFuc);
 
 window.addEventListener("resize", () => {
-    offsetMenuBorder(activeItem, menuBorder);
-    menu.style.setProperty("--timeOut", "none");
+  offsetMenuBorder(activeItem, menuBorder);
+  menu.style.setProperty("--timeOut", "none");
 });
